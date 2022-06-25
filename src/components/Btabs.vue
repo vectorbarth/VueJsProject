343c033e51f9b13d0d7dbfe897380f4c682baa61
Mathis Aymonin <mathis.aymonin@epita.fr>
2022-06-25 21:57:07 +0200

    feat: add color to tabs

--- src/components/Btabs.vue
+++ src/components/Btabs.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="nav nav-tabs">
     <li class="nav-item" v-for="(tabTitle, index) in tabsTitle" :key="index" @click="!tabsDisableStatus[index] ? activeTabIndex = index : {}">
-      <a class="nav-link" :class="{active: index === activeTabIndex, disabled: tabsDisableStatus[index]}">{{ tabTitle }}</a>
+      <a class="nav-link" href="#" :class="{active: index === activeTabIndex, disabled: tabsDisableStatus[index]}">{{ tabTitle }}</a>
     </li>
   </ul>
   <slot/>
