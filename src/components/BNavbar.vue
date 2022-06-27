43ca3e537f9165a2cb377fe3fa137311efdb76c5
Emile <barthemile@gmail.com>
2022-06-27 18:22:05 +0200

    Navbar properties

--- src/components/BNavbar.vue
+++ src/components/BNavbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <nav class="navbar navbar-expand-lg navbar-light" :class="[getVariantClass(), fixedClass, stickyClass]">
+    <a class="navbar-brand" href="/">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,6 +15,35 @@
 <script>
   export default {
     name: "b-navbar",
+    props : {
+      fixed : {
+        type: String,
+      },
+      sticky: {
+        type: Boolean,
+        default: false
+      },
+      variant: {
+        type: String,
+        default: "primary"
+      }
+    },
+    methods : {
+      getVariantClass() {
+        return "bg-" + this.variant
+      }
+    },
+    data() {
+      return {
+        fixedClass: {
+          "fixed-bottom": this.fixed === "bottom",
+          "fixed-top": this.fixed === "top"
+        },
+        stickyClass: {
+          "sticky": this.sticky
+        }
+      }
+    },
     setup (props, {slots}) {
       const navItems = slots.default().find((node) => node.type.name === "b-navbar-nav")
       return {
