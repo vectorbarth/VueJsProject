36a04e2cd69ca1ac537b3bb6f6efda7759cace26
Mathis Aymonin <mathis.aymonin@epita.fr>
2022-06-27 16:55:55 +0200

    feat: implement progress component

--- src/components/BProgress.vue
+++ src/components/BProgress.vue
@@ -1,9 +1,60 @@
 <template>
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" :style="{width: getProcessPercent() + '%'}" :aria-valuenow="value" :aria-valuemin="barMin" :aria-valuemax="barMax" :class="[variantClass, animatedClass, stripedClass]">
+      <a v-if="showProgress">{{ getProcessPercent() }}%</a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "b-progress"
+  name: "b-progress",
+  props: {
+    value: {
+      type: [Number, String],
+      default: 0
+    },
+    variant: {
+      type: String,
+      required: true
+    },
+    showProgress: {
+      type: Boolean,
+      default: false
+    },
+    animated: {
+      type: Boolean,
+      default: false
+    },
+    striped: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      barMin: 0,
+      barMax: 100,
+      variantClass: {
+        "bg-success": this.variant === "success",
+        "bg-info": this.variant === "info",
+        "bg-warning": this.variant === "warning",
+        "bg-danger": this.variant === "danger",
+      },
+      animatedClass: {
+        "progress-bar-animated": this.animated
+      },
+      stripedClass: {
+        "progress-bar-striped": this.striped
+      }
+    }
+  },
+  methods: {
+    getProcessPercent() {
+      const res = Number(100 * Number(this.value) / (this.barMax - this.barMin));
+      return Math.max(this.barMin, Math.min(res, this.barMax))
+    }
+  }
 }
 </script>
 
--- src/views/ProgressView.vue
+++ src/views/ProgressView.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="my-progress">
-    <p>Hello world!</p>
+    <b-progress :value="25" variant="success" striped :animated="animate"></b-progress>
+    <b-progress :value="50" variant="info" striped :animated="animate" class="mt-2"></b-progress>
+    <b-progress :value="75" variant="warning" striped :animated="animate" class="mt-3"></b-progress>
+    <b-progress :value="100" variant="danger" :animated="animate" class="mt-4"></b-progress>
   </div>
 </template>
 
 <script>
+import BProgress from "@/components/BProgress";
 export default {
-  name: "ProgressView"
+  name: "ProgressView",
+  components: {BProgress},
+  data() {
+    return {
+      animate: true
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
