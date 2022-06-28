ac3014d125fe5530ce8fd6e2f306bba086d0d444
Théo GOLGEVIT <theo.golgevit@epita.fr>
2022-06-28 19:01:46 +0200

    refactor: moved table css to BTable.vue

--- src/components/BTable.vue
+++ src/components/BTable.vue
@@ -103,4 +103,32 @@ export default {
 </script>
 
 <style scoped>
+.table>tfoot>tr>[aria-sort],
+.table>thead>tr>[aria-sort] {
+    cursor: pointer;
+    background-image: none;
+    background-repeat: no-repeat;
+    background-size: .65em 1em;
+}
+
+.table>tfoot>tr>[aria-sort],
+.table>thead>tr>[aria-sort] {
+    background-position: right .375rem center;
+    padding-right: calc(.75rem + .65em);
+}
+
+.table>tfoot>tr>[aria-sort="ascending"],
+.table>thead>tr>[aria-sort="ascending"] {
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='101' height='101' preserveAspectRatio='none'%3E%3Cpath d='M51 1l25 23 24 22H1l25-22z'/%3E%3Cpath opacity='.3' d='M51 101l25-23 24-22H1l25 22z'/%3E%3C/svg%3E");
+}
+
+.table>tfoot>tr>[aria-sort="descending"],
+.table>thead>tr>[aria-sort="descending"] {
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='101' height='101' preserveAspectRatio='none'%3E%3Cpath opacity='.3' d='M51 1l25 23 24 22H1l25-22z'/%3E%3Cpath d='M51 101l25-23 24-22H1l25 22z'/%3E%3C/svg%3E");
+}
+
+.table>tfoot>tr>[aria-sort="none"],
+.table>thead>tr>[aria-sort="none"] {
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='101' height='101' preserveAspectRatio='none'%3E%3Cpath opacity='.3' d='M51 1l25 23 24 22H1l25-22zm0 100l25-23 24-22H1l25 22z'/%3E%3C/svg%3E");
+}
 </style>
